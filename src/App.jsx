42a9a48743f4bd5687cbccda260b408bf79054f1
irena binnebesel <irenabinnebesel1@gmail.com>
2024-08-19 14:01:25 +0100

    flashcard folder created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import styles from './App.module.css'
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 import Footer from "./components/Footer/Footer";
+import FlashCards from './components/FlashCards/FlashCards';
+
+
 
 export default function App() {
   return (
     <div className={styles.SuperContainer}>
       <Header />
       <Form />
+      <FlashCards />
       <Footer />
    </div>
   );
---
+++ src/components/FlashCards/FlashCards.jsx
@@ -0,0 +1,11 @@
+
+import styles from './FlashCards.module.css';
+
+export default function FlashCards() {
+
+return (
+  <h1 className={styles.FlashCards}>FlashCards: I am FlashCards </h1>
+)
+};
+
+<FlashCard />
